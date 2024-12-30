c1d8e1139a3dfa6815782aa5a5f0de42042f97c3
Kannan Babu <kannan@MacBookPro.lan>
2024-12-30 16:43:19 -0500

    spring boot application

---
+++ src/main/java/com/example/myclouddemo/MyclouddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclouddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyclouddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myclouddemo/MyclouddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclouddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
